4d5aa60b5627e473d68306aa0aebc347ca6eefe1
zanji17 <zanji17.gs@gmail.com>
2021-04-03 21:25:10 -0300

    v2.0

--- FormPratosProdutos.Designer.cs
+++ FormPratosProdutos.Designer.cs
@@ -31,42 +31,43 @@ namespace Restaurante
         {
             this.txtNome = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.clbIngredientes = new System.Windows.Forms.CheckedListBox();
+            this.clbCategoria = new System.Windows.Forms.CheckedListBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txtTipo = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label3 = new System.Windows.Forms.Label();
+            this.clbIngredientes = new System.Windows.Forms.CheckedListBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(65, 12);
+            this.txtNome.Location = new System.Drawing.Point(53, 9);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(164, 20);
+            this.txtNome.Size = new System.Drawing.Size(229, 20);
             this.txtNome.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 15);
+            this.label1.Location = new System.Drawing.Point(12, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nome";
             // 
-            // clbIngredientes
+            // clbCategoria
             // 
-            this.clbIngredientes.FormattingEnabled = true;
-            this.clbIngredientes.Location = new System.Drawing.Point(27, 89);
-            this.clbIngredientes.Name = "clbIngredientes";
-            this.clbIngredientes.Size = new System.Drawing.Size(202, 214);
-            this.clbIngredientes.TabIndex = 2;
+            this.clbCategoria.FormattingEnabled = true;
+            this.clbCategoria.Location = new System.Drawing.Point(12, 97);
+            this.clbCategoria.Name = "clbCategoria";
+            this.clbCategoria.Size = new System.Drawing.Size(115, 214);
+            this.clbCategoria.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 41);
+            this.label2.Location = new System.Drawing.Point(12, 38);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(28, 13);
             this.label2.TabIndex = 4;
@@ -74,42 +75,53 @@ namespace Restaurante
             // 
             // txtTipo
             // 
-            this.txtTipo.Location = new System.Drawing.Point(65, 38);
+            this.txtTipo.Location = new System.Drawing.Point(53, 35);
             this.txtTipo.Name = "txtTipo";
-            this.txtTipo.Size = new System.Drawing.Size(164, 20);
+            this.txtTipo.Size = new System.Drawing.Size(229, 20);
             this.txtTipo.TabIndex = 3;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(254, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(342, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(454, 291);
+            this.dataGridView1.Size = new System.Drawing.Size(446, 321);
             this.dataGridView1.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 73);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 69);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.Size = new System.Drawing.Size(94, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "Ingredientes";
+            // 
+            // clbIngredientes
+            // 
+            this.clbIngredientes.FormattingEnabled = true;
+            this.clbIngredientes.Location = new System.Drawing.Point(156, 97);
+            this.clbIngredientes.Name = "clbIngredientes";
+            this.clbIngredientes.Size = new System.Drawing.Size(147, 214);
+            this.clbIngredientes.TabIndex = 8;
             // 
             // FormPratosProdutos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 352);
+            this.Controls.Add(this.clbIngredientes);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtTipo);
-            this.Controls.Add(this.clbIngredientes);
+            this.Controls.Add(this.clbCategoria);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtNome);
             this.Name = "FormPratosProdutos";
             this.Text = "Pratos/Produtos";
+            this.Load += new System.EventHandler(this.FormPratosProdutos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -120,11 +132,12 @@ namespace Restaurante
 
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckedListBox clbIngredientes;
+        private System.Windows.Forms.CheckedListBox clbCategoria;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtTipo;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox clbIngredientes;
     }
 }
 
--- FormPratosProdutos.cs
+++ FormPratosProdutos.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Restaurante
 {
     public partial class FormPratosProdutos : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\gusta\\OneDrive\\Documentos\\TCC 2021\\Restaurante\\Restaurante.mdf;Integrated Security=True");
         public FormPratosProdutos()
         {
             InitializeComponent();
+        }
+
+        private void FormPratosProdutos_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            String 
         }
     }
 }
